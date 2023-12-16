6282d5df5275f58d49d479314a5cadd9e07a19b9
Роман Сёмин <prodigy19913@mail.ru>
2023-12-16 17:29:44 +0300

    0.1

---
+++ HideApps/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using Microsoft.Win32;
+
+internal class HideApps
+{
+    #region Fields
+
+    private const int SW_HIDE = 0;
+    private const int MILLISECONDS_TIMEOUT = 100;
+    private const string AUTORUN_PATH = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+    private static readonly string? APPLICATION_NAME = Assembly.GetEntryAssembly()?.GetName().Name;
+    private static readonly string? APPLICATION_PATH = Assembly.GetEntryAssembly()?.Location.Replace(".dll", "");
+
+    #endregion Fields
+
+    #region Methods
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    private static void Main()
+    {
+        AutoRun(true);
+
+        List<string> hideProcesses;
+        using StreamReader reader = new($"{APPLICATION_PATH}.txt");
+        hideProcesses = reader.ReadToEnd().Split("\r\n").ToList();
+
+        for (int id = 0; id < hideProcesses.Count; id++)
+        {
+            Console.WriteLine(hideProcesses[id]);
+        }
+
+        while (hideProcesses.Count > 0)
+        {
+            Thread.Sleep(MILLISECONDS_TIMEOUT);
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (hideProcesses.Contains(process.ProcessName))
+                {
+                    Console.WriteLine(process.ProcessName);
+                    _ = ShowWindow(process.MainWindowHandle, SW_HIDE);
+                    _ = hideProcesses.Remove(process.ProcessName);
+                    if (hideProcesses.Count == 0)
+                        break;
+                }
+            }
+        }
+
+        _ = Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Подключение автозагрузки
+    /// </summary>
+    /// <param name="autorun">подключить?</param>
+    public static void AutoRun(bool autorun = true)
+    {
+        using RegistryKey? registry = Registry.CurrentUser.OpenSubKey(AUTORUN_PATH, true);
+        if (registry == null)
+            return;
+        if (autorun)
+        {
+            if (!registry.GetValueNames().Contains(APPLICATION_NAME))
+                registry.SetValue(APPLICATION_NAME, $"\"{APPLICATION_PATH}.exe\"");
+        }
+        else
+        {
+            if (APPLICATION_NAME == null)
+                return;
+            registry.DeleteValue(APPLICATION_NAME, false);
+        }
+    }
+
+    #endregion Methods
+}
